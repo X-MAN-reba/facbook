efe769f9483d6afa1a679323f30bcb3f988f0ddd
X-MAN-reba <108100590+X-MAN-reba@users.noreply.github.com>
2022-06-24 07:20:39 +0300

    Update pip install .sh

--- pip install .sh
+++ pip install .sh
@@ -2,8 +2,8 @@ pip install requests
 pip install bs4
 pip install random
 pip install re
-pip install 
+pip install subprocess
 pip install webbrowser
 pip install rich
 pip install shutil
-pip install 
+pip install json
